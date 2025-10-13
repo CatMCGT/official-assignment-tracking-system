bc4b60fb82fd9ff37e638bf67433efa5c96549ed
11B 09 LAU Ka Yue <201401111@stu.pkc.edu.hk>
2025-10-13 03:32:18 +0000

    feat(admin): add more components for creating subjects

--- src/app/(main)/admin/subject/CreateSubject.jsx
+++ src/app/(main)/admin/subject/CreateSubject.jsx
@@ -2,13 +2,24 @@ import Radio from "@/components/Radio";
 import Select from "@/components/Select";
 import Form from "next/form";
 import { useState } from "react";
+import { subjectShorthands } from "@/utils/getSubjectInfo";
 
 export default function CreateSubject({ allUsers, allSubjects }) {
   const [subjectType, setSubjectType] = useState("grade");
   const [subjectTeacherId, setSubjectTeacherId] = useState(null);
+  const [subjectShorthand, setSubjectShorthand] = useState(null);
 
   const allTeachers = allUsers?.filter((user) => user.role === "teacher");
   const allStudents = allUsers?.filter((user) => user.role === "student");
+
+  const subjectShorthandOptions = Object.keys(subjectShorthands).map(
+    (shorthand) => {
+      return {
+        id: shorthand,
+        name: subjectShorthands[shorthand],
+      };
+    }
+  );
 
   return (
     <Form className="border-1 border-stroke-weak rounded p-4 w-72">
@@ -43,8 +54,45 @@ export default function CreateSubject({ allUsers, allSubjects }) {
         />
       </div>
 
+      <div className="flex flex-row justify-between items-center mt-3">
+        <label htmlFor="block">
+          Block <span className="text-red-500">*</span>
+        </label>
+        <input
+          type="number"
+          className="w-12 px-1 border-1 border-stroke-weak bg-white rounded focus:outline-1 focus:outline-text-weakest"
+          min="1"
+          max="4"
+          id="block"
+          required
+        />
+      </div>
+
+      <div className="flex flex-col gap-1">
+        <label htmlFor="subjectName">Subject</label>
+        <Select
+          options={subjectShorthandOptions}
+          selected={subjectShorthand}
+          setSelected={setSubjectShorthand}
+          placeholder="No selected subject"
+          allowSearch
+        />
+      </div>
+
       <div className="flex flex-col gap-1">
         <label htmlFor="subjects">Subject Teacher</label>
+        <Select
+          options={allTeachers}
+          selected={subjectTeacherId}
+          setSelected={setSubjectTeacherId}
+          placeholder="No subject teacher"
+          allowSearch
+          showId
+        />
+      </div>
+
+      <div className="flex flex-col gap-1">
+        <label htmlFor="subjects">Enrolled students</label>
         <Select
           options={allTeachers}
           selected={subjectTeacherId}
--- src/utils/getSubjectInfo.js
+++ src/utils/getSubjectInfo.js
@@ -1,33 +1,33 @@
-const subjectShorthands = {
-  chi: 'Chinese',
-  eng: 'English',
-  math: 'Math',
-  cs: 'CS',
-  ls: 'LS',
-  bio: 'Biology',
-  chem: 'Chemistry',
-  phy: 'Physics',
-  ict: 'ICT',
-  chis: 'Chinese History',
-  his: 'History',
-  geo: 'Geography',
-  eco: 'Economy',
-  m1: 'M1',
-  m2: 'M2',
-  art: 'Arts',
-  mus: 'Music',
-  pe: 'PE',
-  epa: 'EPA',
-  lit: 'English literature',
-}
+export const subjectShorthands = {
+  chi: "Chinese",
+  eng: "English",
+  math: "Math",
+  cs: "CS",
+  ls: "LS",
+  bio: "Biology",
+  chem: "Chemistry",
+  phy: "Physics",
+  ict: "ICT",
+  chis: "Chinese History",
+  his: "History",
+  geo: "Geography",
+  eco: "Economy",
+  m1: "M1",
+  m2: "M2",
+  art: "Arts",
+  mus: "Music",
+  pe: "PE",
+  epa: "EPA",
+  lit: "English literature",
+};
 
 export default function getSubjectInfo(subjectId) {
   // Examples of subject ids include: "g11-bio-1", "11b-chi", "g7-chi-t"
 
-  const subjectIdArr = subjectId.split('-')
-  const subjectName = subjectShorthands[subjectIdArr[1]] || 'Unknown'
+  const subjectIdArr = subjectId.split("-");
+  const subjectName = subjectShorthands[subjectIdArr[1]] || "Unknown";
 
-  if (subjectIdArr[0].startsWith('g')) {
+  if (subjectIdArr[0].startsWith("g")) {
     // Subject are grade-based -> have block
 
     return {
@@ -35,13 +35,13 @@ export default function getSubjectInfo(subjectId) {
       name: subjectName,
       block: subjectIdArr[2] || null,
       class: null,
-    }
+    };
   } else {
     return {
       grade: null,
       name: subjectName,
       block: subjectIdArr[2] || null,
       class: subjectIdArr[0],
-    }
+    };
   }
 }
